78c0c40999d406de6c8e51495635fec32204adea
yelaman <67456840+zhanibekov@users.noreply.github.com>
2024-05-13 18:11:46 +0500

    added the ability to delete a goal✅ or task

--- src/index.js
+++ src/index.js
@@ -1,32 +1,18 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import AppHeader from './components/AppHeader';
-import TodoList from './components/TodoList';
-import SearchPanel from './components/SearchPanel';
-import ItemStatusFilter from './components/Item-Status-Filter';
+import ToDo from './ToDo/Todo';
+import ToDoItem from './ToDo/ToDoItem';
 
-const TodoData = [
-    { label: 'How to create react app?', important: false, id: 124 },
-    { label: 'I bought Apple Watch', important: true, id: 145 },
-    { label: 'I broken my Apple Watch', important: true, id: 456 },
-]
 
 const App = () => {
-    return ( <
-        div >
-        <
-        AppHeader / >
-        <
-        ItemStatusFilter / >
-        <
-        SearchPanel / >
-        <
-        TodoList Todos = { TodoData }
-        /> <
-        /div>
+    return ( 
+        <div >
+        <ToDo />
+        <ToDoItem />
+        </div>
 
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( < App / > );
+root.render( < App/> );
